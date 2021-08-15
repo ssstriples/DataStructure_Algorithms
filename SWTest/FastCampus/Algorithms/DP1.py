396b5a2083632ce1ef559c7ba4470b3c6cb926a4
lsh_gram <jplsh76032004@gmail.com>
2021-08-15 13:59:43 +0900

    [FC][Algorithms] DynamicProgramming
    <DESC>
    - 동적 계획법 vs 분할 정복
    - 피보나치 수열 동적 계획법(상향식 방법)

---
+++ SWTest/FastCampus/Algorithms/DP1.py
@@ -0,0 +1,19 @@
+# 재귀
+def fibo(num):
+    if num <= 1:
+        return num
+    return fibo(num - 1) + fibo(num - 2)
+
+print(fibo(4))
+
+# 동적계획법 활용(상향식 방법)
+def fibo_dp(num):
+    cache = [ 0 for index in range(num + 1) ]
+    cache[0] = 0
+    cache[1] = 1
+
+    for index in range(2, num + 1):
+        cache[index] = cache[index - 1] + cache[index - 2]
+    return cache[num]
+
+print(fibo_dp(10))
