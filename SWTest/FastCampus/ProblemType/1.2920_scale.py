29c7568c9189e8d9f4c39ea6e7c664d77e8de022
lsh_gram <jplsh76032004@gmail.com>
2021-08-16 21:42:03 +0900

    [FC][ProblemSolvingType] 자료구조 문제 유형1
    <DESC>
    - 배열 : 음계 2920 / 블랙잭 2798
    - 스택 : 스택 수열 1874
    - 큐 : 프린터 큐 1966

---
+++ SWTest/FastCampus/ProblemType/1.2920_scale.py
@@ -0,0 +1,19 @@
+# 2920 : 음계 (https://www.acmicpc.net/problem/2920)
+
+a = list(map(int, input().split(' ')))
+
+ascending = True
+descending = True
+
+for i in range(1, 8):
+    if a[i] > a[i-1]:
+        descending = False
+    elif a[i] < a[i-1]:
+        ascending = False
+
+if ascending:
+    print('ascending')
+elif descending:
+    print('descending')
+else:
+    print('mixed')
---
+++ SWTest/FastCampus/ProblemType/5.5397_keylogger.py
@@ -0,0 +1,2 @@
+# 5397 : 키로거 (https://www.acmicpc.net/problem/5397)
+
