ed7e73e7155bd39b7f0a48c568c5a754d49906d8
lsh_gram <jplsh76032004@gmail.com>
2021-08-16 17:09:24 +0900

    [FC][Algorithms] BFS, DFS
    <DESC>
    - BFS : 너비 우선 탐색
    - BFS : 큐를 활용한 구현
    - DFS : 깊이 우선 탐색
    - DFS : 스택을 활용한 구현

---
+++ SWTest/FastCampus/Algorithms/DFS1.py
@@ -0,0 +1,29 @@
+def dfs(graph, start_node):
+    visited = list()
+    need_visit = list()
+
+    need_visit.append(start_node)
+
+    while need_visit:
+        node = need_visit.pop() # 인자 없으면 맨끝 데이터 pop
+        
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+
+    return visited
+
+graph = dict()
+
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(dfs(graph, 'A'))
